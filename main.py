8716bbff00023387ed6fce84b01477a1377bab19
Luthfi Khusyasy <Khusyasy@users.noreply.github.com>
2021-03-28 22:40:53 +0700

    add comments

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.content, features="html.parser")
 
+# mengambil ol list nama-nama bayi
 ol = soup.find("ol", {"id": "mntl-sc-block_1-0-13"})
 
+# mengambil 10 nama/li yang berada di dalam ol
 names = [li.text for li in ol.findAll("li")][:10]
 names.sort()
 
